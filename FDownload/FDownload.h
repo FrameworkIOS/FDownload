0098c9ed67c0ea807b86273f54d3ca5bf30d9ea6
krishpranav <krisna.pranav@gmail.com>
2021-11-19 19:41:49 +0530

    add files via upload

---
+++ FDownload/FDownload.h
@@ -0,0 +1,14 @@
+//
+//  FDownload.h
+//  FDownload
+//
+//  Created by Apple on 19/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double FDownloadVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char FDownloadVersionString[];
+
+
---
+++ FDownloadTests/FDownloadTests.m
@@ -0,0 +1,36 @@
+//
+//  FDownloadTests.m
+//  FDownloadTests
+//
+//  Created by Apple on 19/11/21.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FDownloadTests : XCTestCase
+
+@end
+
+@implementation FDownloadTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
