4c4895fdb252912718967fb2530aa2d703126ae0
krishpranav <krisna.pranav@gmail.com>
2021-11-20 06:32:58 +0530

    FDownload: FDownloadTuple

---
+++ FDownload/classes/FDownloadTuple.m
@@ -0,0 +1,28 @@
+//
+//  FDownloadTuple.m
+//  FDownload
+//
+//  Created by Apple on 20/11/21.
+//
+
+#import "FDownloadTuple.h"
+
+
+@implementation FDownloadTuple
+
++ (instancetype)tupleWithDownloadTask:(FDownloadTask *)downloadTask sessionTask:(NSURLSessionDownloadTask *)sessionTask
+{
+    return [[self alloc] initWithDownloadTask:downloadTask sessionTask:sessionTask];
+}
+
+- (instancetype)initWithDownloadTask:(SGDownloadTask *)downloadTask sessionTask:(NSURLSessionDownloadTask *)sessionTask
+{
+    if (self = [super init]) {
+        self.downloadTask = downloadTask
+        self.sessionTask = sessionTask;
+    }
+    
+    return downloadTask;
+}
+
+@end
