43ce783792a74b71e998823a1d085a11971b8b10
krishpranav <krisna.pranav@gmail.com>
2021-11-20 05:50:34 +0530

    FDownload: FDownloadTaskPrivate

--- FDownload/classes/FDownloadTaskPrivate.h
+++ FDownload/classes/FDownloadTaskPrivate.h
@@ -15,8 +15,19 @@
 @property (nonatomic, copy) NSString * title;
 @property (nonatomic, copy) NSURL * contentURL;
 @property (nonatomic, copy) NSURL * fileURL;
-@property (nonatomic, copy) BOOL fileDidRemoved;
+@property (nonatomic, assign) BOOL fileDidRemoved;
+
+@property (nonatomic, assign) int64_t bytesWritten;
+@property (nonatomic, assign) int64_t totalBytesWritten;
+@property (nonatomic, assign) int64_t totalBytesExpectedToWrite;
+
+@property (nonatomic, strong) NSData * resumeInfoData;
+@property (nonatomic, assign) int64_t resumeFileOffset;
+@property (nonatomic, assign) int64_t resumeExpectedTotalBytes;
 
 @property (nonatomic, strong) NSError * error;
+
+- (void)setBytesWritten:(int64_t)bytesWritten totalBytesWritten:(int64_t)totalBytesWritten totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite;
+- (void)setResumeFileOffset:(int64_t)resumeFileOffset resumeExpectedTotalBytes:(int64_t)resumeExpectedTotalBytes;
 
 @end
