5ee2092efb582d45f54c127816bf9db016538a71
krishpranav <krisna.pranav@gmail.com>
2021-11-20 06:23:49 +0530

    FDownload: FDownloadTaskQueu

--- FDownload/classes/FDownloadTaskQueue.m
+++ FDownload/classes/FDownloadTaskQueue.m
@@ -20,3 +20,42 @@
 @property (nonatomic, assign) BOOL closed;
 
 @end
+
+@implementation FDownloadTaskQueue
+
++ (instancetype)queueWithDownload:(FDownload *)download
+{
+    return [[self alloc] initWithDownload:download];
+}
+
+- (instancetype)initWithDownload:(FDownload *)download
+{
+    if (self = [super init]) {
+        self->_download = download;
+        self->_archiverPath = [FDownloadTools archiverFilePathWithIdentifier:download.identifier];
+        self->_tasks = [NSKeyedUnarchiver unarchiveObjectWithFile:self.archiverPath];
+        if (!self->_tasks) {
+            self->_tasks = [NSMutableArray array];
+        }
+        self.condition = [[NSCondition alloc] init];
+        [self resetQueue];
+    }
+    
+    return self;
+}
+
+- (void)resetQueue
+{
+    [self.condition lock];
+    for (FDownloadTask * obj in self.tasks) {
+        obj.download = self.download;
+        if (obj.state == FDownloadTaskStateRunning) {
+            obj.state = FDownloadTaskStateWaiting;
+        }
+    }
+    
+    [self.condition unlock];
+    [self tryArchive];
+}
+
+@end
