c2a1eb286282fac59f9c47b00a2814e8e8bdaff5
krishpranav <krisna.pranav@gmail.com>
2021-11-20 06:09:58 +0530

    FDownload: FDownloadTools

--- FDownload/classes/FDownloadTools.m
+++ FDownload/classes/FDownloadTools.m
@@ -7,14 +7,14 @@
 
 #import "FDownloadTools.h"
 
-static NSString * HomeDirectoryRegexPattern = @"(\\w*-){4}(\\w*)";
+static NSString * HomeDirectoryRegexPattern = @"(\\w*-){4}(\\w*)";      // 98996CAA-7F3B-41A9-A651-9AB15D554E86
 
 @implementation FDownloadTools
 
 + (NSString *)archiverDirectoryPath
 {
     NSString * documentsPath = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES).firstObject;
-    NSString * archiverDirectoryPath = [documentsPath stringByAppendingPathComponent:@"SGDownloadArchive"];
+    NSString * archiverDirectoryPath = [documentsPath stringByAppendingPathComponent:@"FDownloadArchive"];
     BOOL isDirectory;
     BOOL result = [[NSFileManager defaultManager] fileExistsAtPath:archiverDirectoryPath isDirectory:&isDirectory];
     if (!result || !isDirectory) {
@@ -28,13 +28,54 @@ static NSString * HomeDirectoryRegexPattern = @"(\\w*-){4}(\\w*)";
     return [[self archiverDirectoryPath] stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.archive", identifier]];
 }
 
-+ (NSString *)replacehHomeDirectoryForFilePath:(NSString *)filePath
++ (NSURL *)replacehHomeDirectoryForFileURL:(NSURL *)fileURL
 {
     if (!fileURL) return nil;
     
-    NSString * path = [self replacehHomeDirectoryForFilePath:fileURL.path]
-    return [NSURL fileURLwithpath:path];
+    NSString * path = [self replacehHomeDirectoryForFilePath:fileURL.path];
+    return [NSURL fileURLWithPath:path];
+}
+
++ (NSString *)replacehHomeDirectoryForFilePath:(NSString *)filePath
+{
+    if (!filePath) return nil;
+    
+    NSError * error;
+    NSRegularExpression * regex = [NSRegularExpression regularExpressionWithPattern:@"(\\w*-){4}(\\w*)" options:0 error:&error];
+    if (!error) {
+        NSTextCheckingResult * match = [regex firstMatchInString:filePath options:0 range:NSMakeRange(0, filePath.length)];
+        if (match) {
+            NSString * currentName = NSHomeDirectory().lastPathComponent;
+            filePath = [filePath stringByReplacingCharactersInRange:match.range withString:currentName];
+        }
+    }
+    return filePath;
+}
+
++ (NSInteger)sizeWithFileURL:(NSURL *)fileURL
+{
+    if (!fileURL) return 0;
+    
+    NSError * error;
+    NSDictionary <NSFileAttributeKey, id> * attributes = [[NSFileManager defaultManager] attributesOfItemAtPath:fileURL.path error:&error];
+    if (!error || attributes.count > 0) {
+        NSNumber * fileSize = [attributes objectForKey:NSFileSize];
+        return fileSize.integerValue;
+    }
+    return 0;
+}
+
++ (NSError *)removeFileWithFileURL:(NSURL *)fileURL
+{
+    if (!fileURL) return nil;
+    
+    NSError * error = nil;
+    BOOL isDirectory = NO;
+    BOOL result = [[NSFileManager defaultManager] fileExistsAtPath:fileURL.path isDirectory:&isDirectory];
+    if (result && !isDirectory) {
+        result = [[NSFileManager defaultManager] removeItemAtURL:fileURL error:&error];
+    }
+    return error;
 }
 
 @end
-
