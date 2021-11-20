8011ec7ff8a06b9fe9643ffde1023b3fb6cff465
krishpranav <krisna.pranav@gmail.com>
2021-11-20 06:03:27 +0530

    FDownload: FDownloadTools header

---
+++ FDownload/classes/FDownloadTools.h
@@ -0,0 +1,14 @@
+//
+//  FDownloadTools.h
+//  FDownload
+//
+//  Created by Apple on 20/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FDownloadTools : NSObject
+
++ (NSString *)archiverDirectoryPath;
+
+@end 
